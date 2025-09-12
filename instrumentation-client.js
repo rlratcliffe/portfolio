c8266a204f663f70c34e6ba737b4f81935c44105
Rob Ratcliffe <rlr@tcliffe.dev>
2025-09-12 17:15:31 -0500

    chore: turn off cookies

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -2,6 +2,7 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  cookieless_mode: 'always',
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
 });
