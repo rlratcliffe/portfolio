ce50c6f953b31d351480f7a26626a0f5d803e81b
Rob Ratcliffe <rlr@tcliffe.dev>
2025-09-12 17:55:31 -0500

    fix: build typing issues

--- src/app/domain/SoftwareProjects.ts
+++ src/app/domain/SoftwareProjects.ts
@@ -1,13 +1,11 @@
-import {StaticImageData} from "next/image";
-
 export interface SoftwareProject {
     id: number;
     title: string;
-    img: StaticImageData;
+    img?: string;
     description: string;
     technologies: string[];
-    github: string;
-    demo: string
+    github?: string;
+    demo?: string
 }
 
 export class SoftwareProjects {
@@ -18,15 +16,12 @@ export class SoftwareProjects {
             description: "This portfolio!",
             technologies: ["Next.js"],
             github: "https://github.com/rlratcliffe/portfolio",
-            demo: ""
         },
         {
             id: 2,
             title: "DataSonnet Playground",
             description: "A programming playground to experiment with the DataSonnet transformation language.",
             technologies: ["React", "JavaScript"],
-            github: "",
-            demo: "",
             img: "ds-playground.png"
         }
     ];
